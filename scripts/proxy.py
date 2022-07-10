bb015f987bda47a90b8aca06704d5a10ada7f4fd
Justin Pham <justinpham79@outlook.com>
2022-07-10 01:52:53 -0700

    use local install of babel instead of global

--- scripts/proxy.py
+++ scripts/proxy.py
@@ -73,7 +73,7 @@ def processFile (flow, content, ext):
             # if js, then run babel to convert to es5
             if fileName[len(fileName) - 3 :] == ".js" or fileName[len(fileName) - 4 :] == ".jsx":
                 print("*** Running Babel", fileName)
-                babel_command = "babel " + fileName + " --out-file " + fileName
+                babel_command = "./node_modules/.bin/babel " + fileName + " --out-file " + fileName
                 # ret = os.system(babel_command)
                 ret = subprocess.check_output(babel_command, shell=True)
                 print("** Babel Done:", ret, fileName)
